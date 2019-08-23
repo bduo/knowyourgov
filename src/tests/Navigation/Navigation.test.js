1251a8285dd7d0eabaeda3823b1b4585515ee671
Brandon El Haj <brandon.elhaj@gmail.com>
2019-08-22 20:18:40 -0700

    Updated tests

--- src/tests/Navigation/Navigation.test.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-// import Navigation from '../../components/Navigation/Navigation';
-// import ReactDOM from 'react-dom'
-
-// it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<Navigation/>, div);
-//     ReactDOM.unmountComponentAtNode(div);
-// });
