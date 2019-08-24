df4fcff5c56421b95cba2d4549db11fa8410c1a0
Brandon El Haj <brandon.elhaj@gmail.com>
2019-08-24 00:31:41 -0700

    Updated

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    REACT_APP_API_ENDPOINT: 'http://localhost:8000/api',
+    REACT_APP_API_ENDPOINT: 'https://frozen-mesa-71139.herokuapp.com/api',
     REACT_APP_TOKEN_KEY: 'knowyourgov-auth-token',
     REACT_APP_CIVIC_API_KEY: process.env.REACT_APP_CIVIC_API_KEY,
   }
